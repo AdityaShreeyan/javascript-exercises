ebc15f2b4e751b58f8f7af3cd34dd917b3cae3bf
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-09-02 19:24:22 +0530

    Passed the first test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    let sameString = '';
+    for (i = 0; i<num; i++){
+        sameString +=`${string}`;
+    }
+    return sameString;
+}
+repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
