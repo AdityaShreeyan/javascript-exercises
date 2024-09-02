a6dc1324d4f957c753938a9c6faf7d8707776995
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-09-02 19:41:12 +0530

    Modified the script to pass all the tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,8 +3,12 @@ const repeatString = function(string, num) {
     for (i = 0; i<num; i++){
         sameString +=`${string}`;
     }
+    if (num<0){
+        return 'ERROR'
+    }
+    else{
     return sameString;
+    }
 }
-repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
