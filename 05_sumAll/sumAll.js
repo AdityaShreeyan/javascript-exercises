a48a6c756973776a9b5d8d00338c3daecba34fd4
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-09-03 13:11:29 +0530

    Changed the script to pass all the tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function(num1,num2) {
     let sum = 0;
-    if (num1 < 0){
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' 
+        || !Number.isInteger(num1) || !Number.isInteger(num2) 
+        || num1 < 0 || num2 < 0) {
         return 'ERROR';
     }
     else if (num1>num2){
