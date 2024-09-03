ff7c079bec281ee1c5c0f304c6fd5bc977cf417a
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-09-03 15:15:47 +0530

    Changed the script to pass all the tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase().replace(/[^a-z0-9]/gi,'');
+    let reverse = '';
+    for (let i = cleanString.length - 1; i >= 0; i--){
+        reverse += cleanString.charAt(i);
+    }
+    if (cleanString === reverse){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
