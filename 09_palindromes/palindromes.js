3239fe0085495e88359fb664259b35e29f13966d
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-09-03 15:21:05 +0530

    Changed the script to pass all the tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,15 +1,10 @@
 const palindromes = function (string) {
     const cleanString = string.toLowerCase().replace(/[^a-z0-9]/gi,'');
-    let reverse = '';
+    let reversedString = '';
     for (let i = cleanString.length - 1; i >= 0; i--){
-        reverse += cleanString.charAt(i);
+        reversedString += cleanString.charAt(i);
     }
-    if (cleanString === reverse){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return reversedString === cleanString;
 };
 
 // Do not edit below this line
